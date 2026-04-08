a9d04f4fb5f913b7e96e86e21ec8e89962415a28
Enzo Coranson <enzomarcuscb@gmail.com>
2026-04-08 02:58:36 +0200

    Create attitude.h

---
+++ Core/Inc/attitude.h
@@ -0,0 +1,101 @@
+
+#ifndef ATTITUDE_H
+#define ATTITUDE_H
+
+#include "imu.h"
+
+
+/*
+ * de imu.h attitude.h peut utiliser les structures suivantes
+ *
+typedef enum {
+
+	euler_roll = 0,
+	euler_pitch,
+	euler_yaw,
+	angle_count               //=3
+
+} euler_angle_t;          //index des differant axes autour du quel tourne le drone
+
+
+typedef enum {
+
+	axis_x = 0,
+	axis_y,
+	axis_z,
+	axis_count  //=3
+
+}axis_t;
+ *
+ */
+
+typedef enum {
+	quat_index_w = 0,
+	quat_index_x,
+	quat_index_y,
+	quat_index_z,
+	quat_count,
+}quaternion_index_t;
+
+
+typedef struct {
+	float quat_w;
+	float quat_x;
+	float quat_y;
+	float quat_z;
+}quaternion_t;
+
+typedef struct {
+	quaternion_t quat;
+	float euler_angle[angle_count];
+	bool is_attitude_valid;
+
+
+} attitude_t;
+
+
+typedef struct {
+	float dt;
+	float norme_accel;
+	float norme_quat;
+	float gyro;
+	float gyro_temp[axis_count];
+} is_valid_attitude_t;
+
+
+
+
+
+/////////////////////////////////////initialisation et reset ///////////////////////////////////////////
+void attitude_init(void);  //initialise le module : angles, quaternions, filtre
+
+void attitude_reset(void); //reboot
+
+
+
+
+//////////////////////////////////////optention et validation des donnes///////////////////////////////////////
+ //reception des donnes imu, correction/conversion, mise a jour de l attitude
+void attitude_update(void);
+
+
+bool attitude_is_valid(void);
+
+
+//////////////////////////////////////////getter////////////////////////////////
+const attitude_t* attitude_get(void);
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
