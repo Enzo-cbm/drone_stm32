2da0928cc23a474a7cd3f5f9f8658c20562d08ac
Enzo Coranson <enzomarcuscb@gmail.com>
2026-04-16 03:52:05 +0200

    Refactor set_point.c for improved variable management

--- Core/Src/set_point.c
+++ Core/Src/set_point.c
@@ -12,29 +12,46 @@ static const struct {
 	float down_mid_lim;
 	float upper_mid_lim;
 	float upper_lim;
-	float max_degre_sec;
-	float min_degre_sec;
+	float max_degre_sec_roll_pitch;
+	float max_degre_sec_yaw;
+	float max_angle_deg;
 	float throttle_lim;
-	float yaw_deadband;
-	float yaw_hold_gain;
-	float coef_stab;
 } var_consigne = {
-		.low_lim       = 1000.0f,
-		.down_mid_lim  = 1492.0f,
-		.upper_mid_lim = 1508.0f,
-		.upper_lim     = 2000.0f,
-		.max_degre_sec = 95.0f,
-		.min_degre_sec = -95.0f,
-		.throttle_lim  = 1050.0f,
-		.yaw_deadband  = 2.0f,
-		.yaw_hold_gain = 2.0f,
-		.coef_stab     = 2.0f,
+		.low_lim         = 1000.0f,
+		.down_mid_lim    = 1492.0f,
+		.upper_mid_lim   = 1508.0f,
+		.upper_lim       = 2000.0f,
+		.max_degre_sec_roll_pitch = 200.0f,   //95.0f
+		.max_degre_sec_yaw = 130.0f,
+		.max_angle_deg = 35.0f,
+		.throttle_lim    = 1050.0f,
+
 };
 
-static float yaw_target;
 
 
-//maping des pulse radio -> degre/sec : [1000, 2000] -> [min_degre_sec , max_degre_sec ]
+
+
+
+
+
+
+static void consigne_reset(consigne_t *consigne){
+
+	memset(consigne, 0, sizeof(*consigne));
+
+}
+
+
+void consigne_init(consigne_t *consigne){
+	consigne_reset(consigne);
+}
+
+
+
+
+
+//maping des pulse radio -> degre/sec : [1000, 2000] -> [min_degre_sec , max_degre_sec ] ou vers -> [min_degre_angle , max_degre_angle ]
 static inline float map_f(float x, float in_min, float in_max, float out_min, float out_max){
 
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min ;
@@ -43,102 +60,93 @@ static inline float map_f(float x, float in_min, float in_max, float out_min, fl
 
 
 
-static void consigne_reset(consigne_t *consigne, const attitude_t *att){
 
-	memset(consigne, 0, sizeof(*consigne));
-	yaw_target = att->euler_angle[euler_yaw];
+//fonction pour normaliser les pulse [1000, 2000] -> [-1, 1]
+static inline float radio_to_stick_norm_deadband(float pulses){
 
-}
+	if(pulses > var_consigne.upper_mid_lim) {
 
-
-void consigne_init(consigne_t *consigne, const attitude_t *att){
-	consigne_reset(consigne, att);
-}
-
-
-static void set_point_from_radio_roll_pitch(consigne_t *consigne, const radio_raw_t *radio_pulses, const attitude_t *att){
-
-	//ROLL
-	if (radio_pulses->pulse_us[ch1] > var_consigne.upper_mid_lim) {
-		consigne->cmd[ROLL] = map_f(radio_pulses->pulse_us[ch1], var_consigne.upper_mid_lim, var_consigne.upper_lim, 0, var_consigne.max_degre_sec);
-	} else if (radio_pulses->pulse_us[ch1] < var_consigne.down_mid_lim) {
-		consigne->cmd[ROLL] = map_f(radio_pulses->pulse_us[ch1], var_consigne.low_lim, var_consigne.down_mid_lim, var_consigne.min_degre_sec, 0);
-	} else {
-		consigne->cmd[ROLL] = 0.0f;
+		return map_f(pulses, var_consigne.upper_mid_lim, var_consigne.upper_lim, 0.0f, 1.0f);
 	}
+	else if( pulses < var_consigne.down_mid_lim ) {
+		return map_f(pulses, var_consigne.low_lim, var_consigne.down_mid_lim, -1.0f, 0.0f);
 
-	//PITCH inverse
-	if (radio_pulses->pulse_us[ch2] > var_consigne.upper_mid_lim) {
-			consigne->cmd[PITCH] = map_f(radio_pulses->pulse_us[ch2], var_consigne.upper_mid_lim, var_consigne.upper_lim, 0, var_consigne.min_degre_sec);
-		} else if (radio_pulses->pulse_us[ch2] < var_consigne.down_mid_lim) {
-			consigne->cmd[PITCH] = map_f(radio_pulses->pulse_us[ch2], var_consigne.low_lim, var_consigne.down_mid_lim, var_consigne.max_degre_sec, 0);
-		} else {
-			consigne->cmd[PITCH] = 0.0f;
-		}
-
-	consigne->cmd[ROLL]  -= att->euler_angle[euler_roll] * var_consigne.coef_stab ;
-	consigne->cmd[PITCH] -= att->euler_angle[euler_pitch] * var_consigne.coef_stab ;    //attention au bornage, a modifier
-
+	}else { return 0.0f;}
 
 }
 
-//reinitialiser yaw_target lorsque throttle est bas pour eviter un pic
-//amelioration du hold avec le futur magnetometre
-static void set_point_from_radio_yaw(consigne_t *consigne, const radio_raw_t *radio_pulses, const attitude_t *attitude_1){
 
-	float cmd_yaw = 0.0f;
 
-	//yaw pris en compte uniquement si throttle > seuil
 
-	if (radio_pulses->pulse_us[ch3] > var_consigne.throttle_lim ) {
-		if (radio_pulses->pulse_us[ch4] > var_consigne.upper_mid_lim) {
-			cmd_yaw = map_f(radio_pulses->pulse_us[ch4], var_consigne.upper_mid_lim, var_consigne.upper_lim, 0, var_consigne.max_degre_sec);
-		}else if (radio_pulses->pulse_us[ch4] < var_consigne.down_mid_lim) {
-			cmd_yaw = map_f(radio_pulses->pulse_us[ch4], var_consigne.low_lim, var_consigne.down_mid_lim, var_consigne.min_degre_sec, 0);
-		} else {
-			cmd_yaw = 0.0f;
-		}
 
-		//en cas de stick centre on veut un hold
-		if (fabsf(cmd_yaw) < var_consigne.yaw_deadband)
-		{
-			float yaw_error = yaw_target - attitude_1->euler_angle[euler_yaw];
 
-				//normalisation
-			if (yaw_error > 180.0f) {yaw_error -= 360.0f; }
-			if (yaw_error < -180.0f) {yaw_error += 360.0f; }
 
-			consigne->cmd[YAW] = yaw_error * var_consigne.yaw_hold_gain;
 
-		} else {
-			consigne->cmd[YAW] = cmd_yaw;
-			yaw_target = attitude_1->euler_angle[euler_yaw];
-		}
 
+//optenire les conversions des pulses roll et pitch et deg_sec ou en degre
+static void set_point_from_radio_roll_pitch(consigne_t *consigne, const radio_raw_t *radio_pulses) {
+
+	consigne->stick_norm[ROLL]  = radio_to_stick_norm_deadband(radio_pulses->pulse_us[ch1]);
+	consigne->stick_norm[PITCH] = -radio_to_stick_norm_deadband(radio_pulses->pulse_us[ch2]);  //pitch : axe inverse
+
+	//rate set_point
+	consigne->rate_sp[ROLL]     = consigne->stick_norm[ROLL] * var_consigne.max_degre_sec_roll_pitch;
+	consigne->rate_sp[PITCH]    = consigne->stick_norm[PITCH] * var_consigne.max_degre_sec_roll_pitch;
+
+	//angle set_point
+	consigne->angle_sp[ROLL]     = consigne->stick_norm[ROLL] * var_consigne.max_angle_deg;
+	consigne->angle_sp[PITCH]    = consigne->stick_norm[PITCH] * var_consigne.max_angle_deg;
+
+}
+
+
+//conversion des pulses yaw pas de convesion en angle car pas d'angle absolu comme roll ou pitch
+static void set_point_from_radio_yaw(consigne_t *consigne, const radio_raw_t *radio_pulses) {
+
+
+	if(radio_pulses->pulse_us[ch3] > var_consigne.throttle_lim) {
+
+		consigne->stick_norm[YAW] = radio_to_stick_norm_deadband(radio_pulses->pulse_us[ch4]);
 	}else {
-		consigne->cmd[YAW] = 0.0f;
+	    consigne->stick_norm[YAW] = 0.0f;
 	}
 
+	//rate set_point
+	consigne->rate_sp[YAW]     = consigne->stick_norm[YAW] * var_consigne.max_degre_sec_yaw;
 
 }
 
+
+
+
+//sauvegarde des pulse throttle tel quel avec une limite a 1700 us pour laisser de la marge au PID
 static void set_point_from_radio_throttle(consigne_t *consigne, const radio_raw_t *radio_pulses) {
 
-	consigne->cmd[THROTTLE] = radio_pulses->pulse_us[ch3];
+	//consigne->stick_norm[THROTTLE] = radio_to_stick_norm(radio_pulses->pulse_us[ch3]);
 
-	if (consigne->cmd[THROTTLE] > 1700.0f) { consigne->cmd[THROTTLE] = 1700.0f;}
+	// limitation throttle pour garder de la marge aux corrections PID
+	consigne->cmd_throttle_us = radio_pulses->pulse_us[ch3];
+	if (consigne->cmd_throttle_us> 1700.0f) { consigne->cmd_throttle_us = 1700.0f;}
+
 
 }
 
-void set_point_update_from_radio(consigne_t *consigne, const radio_raw_t *radio_pulses, const attitude_t *att){
 
-	set_point_from_radio_roll_pitch(consigne, radio_pulses, att);
-	set_point_from_radio_yaw(consigne, radio_pulses, att);
+
+
+
+void set_point_update_from_radio(consigne_t *consigne, const radio_raw_t *radio_pulses){
+
+	set_point_from_radio_roll_pitch(consigne, radio_pulses);
+	set_point_from_radio_yaw(consigne, radio_pulses);
 	set_point_from_radio_throttle(consigne, radio_pulses);
 }
 
 
+/*
+static inline float radio_to_stick_norm(float pulses) {
 
-
-
+	return map_f(pulses, var_consigne.low_lim, var_consigne.upper_lim, 0, 1);
+}
+*/
 
